e0f6d2f2ab7922ec0d0d51b02b1b85bdaaca8b6e
yutarohabu <hup0628@gmail.com>
2024-09-17 11:08:05 +0900

    change:分割代入の練習問題回答完了

--- src/lesson/splitAssignment.js
+++ src/lesson/splitAssignment.js
@@ -1,7 +1,23 @@
 // ここにコードを書きながら確認しましょう！
+const array = ['one', 'two', 'three'];
+const [ hoge, fuga, piyo ] = array;
 
+console.log(hoge); // one
+console.log(fuga); // two
+console.log(piyo); // three
 
+const gizumo = {
+  name: 'Gizumo',
+  established: '2015年5月15日',
+  place: 'Shibuya',
+};
 
+// 分割代入
+const { name, established, place } = gizumo;
+
+console.log(name); // Gizumo
+console.log(established); // 2015年5月15日
+console.log(place); // Shibuya
 
 
 
@@ -17,9 +33,14 @@ function arrayFn(arg) {
   return ['jQuery', 'Vue', arg];
 }
 
+const [jQuery,Vue,arg] = arrayFn(10);
+
+console.log(arg);
+
 // 問題 2
-function objectFn() {
-  console.log();
+function objectFn(name) {
+  const {name: companyName} = argObject;
+  console.log(companyName);
 }
 
 const argObject = {
